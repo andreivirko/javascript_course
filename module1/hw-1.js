713b0d6507efadeac1e2c229d767105e7a93621e
andreivirko <andreithebro231@gmail.com>
2020-03-19 12:43:43 +0200

    js.hw-1 is done

--- module1/hw-1.js
+++ module1/hw-1.js
@@ -5,16 +5,103 @@
 // выведи каждое значение этой переменной в консоль
 // присвой 10 разных значений
 
+let a;
+a = "monkey";
+console.log(a);
+console.log(typeof a);
+a = "tiger";
+console.log(a);
+console.log(typeof a);
+a = "lion";
+console.log(a);
+console.log(typeof a);
+a = "elephant";
+console.log(a);
+console.log(typeof a);
+a = "leopard";
+console.log(a);
+console.log(typeof a);
+a = "dog";
+console.log(a);
+console.log(typeof a);
+a = "cat";
+console.log(a);
+console.log(typeof a);
+a = "rat";
+console.log(a);
+console.log(typeof a);
+a = "mouse";
+console.log(a);
+console.log(typeof a);
+a = "racoon";
+console.log(a);
+console.log(typeof a);
+
 // ========= 2 =========
 // объяви переменную со значением своего имени, которую нельзя перезаписать
+const myFirstName = "Andrei";
+console.log(myFirstName);
 
 // ========= 3 =========
 // создай 5 переменных с названиями любимых песен, так, чтобы их нельзя было изменить
+const theBestSong1 = "Godzilla";
+console.log(theBestSong1);
+const theBestSong2 = "Bandit";
+console.log(theBestSong2);
+const theBestSong3 = "Thotiana";
+console.log(theBestSong3);
+const theBestSong4 = "Suey";
+console.log(theBestSong4);
+const theBestSong5 = "Suge";
+console.log(theBestSong5);
+
 
 // ========= 4 =========
-// создай разные переменные сщ значениями: 3, "Студент", true, undefind, null 
+// создай разные переменные с значениями: 3, "Студент", true, undefined, null 
 // выведи значения переменных и тип данных каждого значения в консоль
+let newVar = 3;
+console.log(newVar);
+console.log(typeof newVar);
+
+newVar = "student";
+console.log(newVar);
+console.log(typeof newVar);
+
+newVar = true;
+console.log(newVar);
+console.log(typeof newVar);
+
+newVar = undefined;
+console.log(newVar);
+console.log(typeof newVar);
+
+newVar = null;
+console.log(newVar);
+console.log(typeof newVar);
+
+
 
 // ========= 5 =========
 // проверь на число каждое значение переменной, созданной в предыдущем задании
 // и выведи это в консоль
+
+newVar = 3;
+console.log(newVar);
+console.log(isNaN(newVar)); // false
+
+newVar = "student";
+console.log(newVar);
+console.log(isNaN(newVar)); // true
+
+newVar = true;
+console.log(newVar);
+console.log(isNaN(newVar)); // false cos it become a number 1
+
+newVar = undefined;
+console.log(newVar);
+console.log(isNaN(newVar)); // true cos it become a NaN
+
+newVar = null;
+console.log(newVar);
+console.log(isNaN(newVar)); // false cos it become to 0
+
--- module1/hw-2.js
+++ module1/hw-2.js
@@ -1,19 +1,68 @@
 "use strict";
 
-// ========= 1 =========
-// выведи в консоль значения всех примитивных типов данных
+// // ========= 1 =========
+// // выведи в консоль значения всех примитивных типов данных
+// console.log(Boolean);
+// console.log(null);
+// console.log(undefined);
+// console.log(String);
+// console.log(Object);
 
-// ========= 2 =========
-// Приведи к числу следующие значения: false, true, "253", "JavaScript", undefined, null
+// // ========= 2 =========
+// // Приведи к числу следующие значения: false, true, "253", "JavaScript", undefined, null
+// let = false;
+// console.log(Number(false));
+// let = true;
+// console.log(Number(true));
+// let = 253;
+// console.log(Number.parseInt("253px"));
+// let = javascript;
+// console.log(Number(javascript));
+// let = undefined;
+// console.log(Number(undefined));
+// let = null;
+// console.log(Number(null));
 
-// ========= 3 =========
-// Приведи к булю следующие значения: '', "  ", "253", "JavaScript", undefined, null
 
-// ========= 4 =========
-// Приведи к строке следующие значения: 214, undefined, null, true, false
 
-// ========= 5 =========
-// Сравни следующие значения: 4, 3 и false, так, чтобы вернулся ответ true.
-// Сравни следующие значения: 4, 3 и false, так, чтобы вернулся ответ false.
-// Напиши все варианты, которые у тебя получились
+
+
+// // ========= 3 =========
+// // Приведи к булю следующие значения: '', "  ", "253", "JavaScript", undefined, null
+// let = ' ';
+// console.log(Boolean(' '));
+// let = " ";
+// console.log(Boolean(" "));
+// let = 253;
+// console.log(Boolean(253));
+// let = Javascript;
+// console.log(Boolean(Javascript));
+// let = undefined;
+// console.log(Boolean(undefined));
+// let = null;
+// console.log(Boolean(null));
+
+
+// // ========= 4 =========
+// // Приведи к строке следующие значения: 214, undefined, null, true, false
+// let = 214;
+// console.log(String(214));
+// let = undefined;
+// console.log(String(undefined));
+// let = null;
+// console.log(String(null));
+// let = true;
+// console.log(String(true));
+// let = false;
+// console.log(String(false));
+
+// // ========= 5 =========
+// // Сравни следующие значения: 4, 3 и false, так, чтобы вернулся ответ true.
+// // Сравни следующие значения: 4, 3 и false, так, чтобы вернулся ответ false.
+// // Напиши все варианты, которые у тебя получились
+// console.log(4 > 3 == 1);
+// console.log(!false);
+// console.log(4 > 3 === 1);
+// console.log(false === 0);
+
 
