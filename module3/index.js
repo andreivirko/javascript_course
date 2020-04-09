625be9be2a66885e15d952afb5294c04ce0de549
andreivirko <andreithebro231@gmail.com>
2020-04-09 09:24:53 +0300

    module3

---
+++ module3/index.js
@@ -0,0 +1,50 @@
+"use strict";
+
+// OBJECT
+
+// let abc = {
+//     1: "a",
+//     2: "b",
+//     3: "c", // property = key : value
+//     // key : value
+// } 
+
+// console.log(abc);
+
+// let language = {
+//     lang1: "html",
+//     lang2: "css",
+//     lang3: "js"
+
+// }
+
+// let student = {
+//     name: "Bob",
+//     lastName: "Dylan",
+//     age: 17,
+//     language: language,
+//     passExam: true,
+//     size: ["s", "m", "l"]
+// } 
+
+// console.log(student);
+
+// let girl = {
+//     name: "natasha",
+//     age: 15,
+
+// }
+
+// console.log(girl.name);
+// console.log(girl.age);
+// console.log(girl.address); // udefined because in this object havent got this property
+// girl.address = "Lenina 13"; // added new property
+// console.log(girl.address);
+// console.log(girl);
+// girl.age = 22; // the value of property is changed
+// console.log(girl.age); 
+// console.log(girl);
+
+// delete girl.age; // to delete the property
+// console.log(girl.age); // undefined, because this property was deleted before
+// console.log(girl);
